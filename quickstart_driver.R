0fd13dcfe9c9ac63b176f60711735de67e2926d7
Cathleen Balantic <cbalantic@gmail.com>
2017-05-03 15:02:34 -0400

    added github installs

--- quickstart_driver.R
+++ quickstart_driver.R
@@ -1,3 +1,13 @@
 library(swirlify)
-set_lesson()
-swirlify("My First Lesson", "My New Course")
+
+# new_lesson("Lab_1", "Fledglings") # create a new lesson
+new_lesson("Lab_1_tester", "Fledglings") 
+
+# Open an existing lesson
+set_lesson('./Fledglings/Lab_1/lesson.yaml')
+set_lesson('./Fledglings/Lab_1_tester/lesson.yaml')
+
+demo_lesson()
+
+
+install_course_github('cbalantic', 'Fledglings')
